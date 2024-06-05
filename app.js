911430a3261d9d4cc4463eff1198abaa1272066e
KEastWiseman <2ehgus6260@gmail.com>
2024-06-05 16:48:27 +0900

    hotfix : error catch

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv/config";
-import CashRouter from "./routers/cash.router";
+import UserRouter from './routers/user.router.js';
+import CashRouter from "./routers/cash.router.js";
 import GameRouter from "./routers/game.router.js";
 
 const app = express();
@@ -8,7 +9,10 @@ const PORT = process.env.PORT || 8081;
 
 app.use(express.json());
 
-app.use("/api", [CashRouter, GameRouter]);
+app.use("/api", [
+  UserRouter,
+  CashRouter, 
+  GameRouter]);
 
 app.use("/", async (req, res, next) => {
   res.send("futsal Online project from NBC 힘순조");
--- routers/user.router.js
+++ routers/user.router.js
@@ -1,38 +1,36 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import jwt from 'jsonwebtoken';
-import { futsalPrisma } from '../util/prisma/index.js';
+import { prisma } from '../util/prisma/index.js';
 import dotenv from "dotenv";
 
 dotenv.config();
-const app = express();
+const router = express.Router();
 const SECRET_KEY = 'supersecretkey';
-const userIdRegex = /^[a-z0-9]+$/;
+const accountNameRegex = /^[a-z0-9]+$/;
 const passwordRegex = /^[a-zA-Z\d\W]{6,}$/
-const jwtSecret = process.env.JWT_SECRET;
-
-app.use(bodyParser.json());
+const jwtSecret = process.env.USER_TOKEN_KEY;
 
 // 회원가입 API
-app.post('/api/sign-up', async (req, res) => {
-    const { userId, password } = req.body;
+router.post('/sign-up', async (req, res) => {
+    const { accountName, password } = req.body;
 
-    // userId와 password가 모두 전달되었는지 확인
-    if (!userId || !password) {
-        return res.status(400).json({ message: "userId 또는 password를 입력하세요" });
+    // accountName와 password가 모두 전달되었는지 확인
+    if (!accountName || !password) {
+        return res.status(400).json({ message: "accountName 또는 password를 입력하세요" });
     }  
     // ID 조건 (영소문자 + 숫자로만 구성하기)   
-    if (!userIdRegex.test(userId)) {
+    if (!accountNameRegex.test(accountName)) {
         return res.status(400).json({ message: "영소문자와 숫자로만 입력하세요" });
     }
     // 비밀번호 조건 (6자리이상) 
     if (!passwordRegex.test(password)) {
         return res.status(400).json({ message: "6자 이상으로 입력하세요" });
     }
-    // userID 중복 체크
-    const userExistChecker = await futsalPrisma.user.findFirst({
+    // accountName 중복 체크
+    const userExistChecker = await prisma.user.findFirst({
         where:{
-            userId,
+            accountName,
         }
     })
 
@@ -42,14 +40,14 @@ app.post('/api/sign-up', async (req, res) => {
 
     try {
         // 사용자 생성
-        const newUser = await futsalPrisma.user.create({
+        const newUser = await prisma.user.create({
             data: {
-                userId,
+                accountName,
                 password,
                 cash: 10000, // 캐시 초기값
             },
         });
-        res.status(201).json({ message: `${userId}로 가입되었습니다`, createdAt: new Date() });
+        res.status(201).json({ message: `${accountName}로 가입되었습니다`, createdAt: new Date() });
     } catch (error) {
         console.error('회원가입 중 오류 발생:', error);
         res.status(500).json({ message: '서버 오류' });
@@ -57,27 +55,27 @@ app.post('/api/sign-up', async (req, res) => {
 });
 
 // 로그인 API
-app.post('/api/sign-in', async (req, res) => {
-    const { userId, password } = req.body;
+router.post('/sign-in', async (req, res) => {
+    const { accountName, password } = req.body;
 
-    // userId와 password가 모두 전달되었는지 확인
-    if (!userId || !password) {
-        return res.status(400).json({ message: "userId 또는 password를 입력하세요" });
+    // accountName password가 모두 전달되었는지 확인
+    if (!accountName || !password) {
+        return res.status(400).json({ message: "accountName 또는 password를 입력하세요" });
     }
 
     try {
         // 사용자 조회
-        const user = await futsalPrisma.user.findUnique({
+        const user = await prisma.user.findUnique({
             where: {
-                userId,
+                accountName,
             },
         });
 
         // 사용자가 존재하고 비밀번호가 일치하는 경우
         if (user && user.password === password) {
-            const token = jwt.sign({ userId: user.userId }, jwtSecret);
-            res.header('Authirization', `Bearer ${token}`).status(200).json({ 
-                message: `${userId}님 환영합니다`, 
+            const token = jwt.sign({ accountName: user.accountName }, jwtSecret);
+            res.header('Authorization', `Bearer ${token}`).status(200).json({ 
+                message: `${accountName}님 환영합니다`, 
                 cash: user.cash, 
                 createdAt: new Date(), 
                 token 
@@ -90,3 +88,5 @@ app.post('/api/sign-in', async (req, res) => {
         res.status(500).json({ message: '서버 오류' });
     }
 });
+
+export default router;
